04e2783bf982ad576c5bd7ad9ca94bd374bc9c5e
sylvain PEQUIGNOT <sylvain.pequignot@vetoquinol.com>
2017-11-16 15:38:00 +0100

    adding controller support. Still have to code it though.

--- core/src/com/runner/arthur/dora/RunnerDora.java
+++ core/src/com/runner/arthur/dora/RunnerDora.java
@@ -4,6 +4,9 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.controllers.Controller;
+import com.badlogic.gdx.controllers.ControllerListener;
+import com.badlogic.gdx.controllers.PovDirection;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -15,11 +18,15 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import java.util.ArrayList;
 
-public class RunnerDora extends ApplicationAdapter {
+import javax.sound.midi.ControllerEventListener;
+
+public class RunnerDora extends ApplicationAdapter implements ControllerListener {
+
 	public static OrthographicCamera camera;
 	public static ArrayList<Item> items = new ArrayList();
 	public static GameStatus status = GameStatus.LOADING;
@@ -367,5 +374,50 @@ public class RunnerDora extends ApplicationAdapter {
 		this.win_text_height = layout.height;
 		this.x_size_factor = ((float) Gdx.graphics.getWidth()) / this.viewport.getWorldWidth();
 		this.y_size_factor = ((float) Gdx.graphics.getHeight()) / this.viewport.getWorldHeight();
+	}
+
+	@Override
+	public void connected(Controller controller) {
+
+	}
+
+	@Override
+	public void disconnected(Controller controller) {
+
+	}
+
+	@Override
+	public boolean buttonDown(Controller controller, int buttonCode) {
+		return false;
+	}
+
+	@Override
+	public boolean buttonUp(Controller controller, int buttonCode) {
+		return false;
+	}
+
+	@Override
+	public boolean axisMoved(Controller controller, int axisCode, float value) {
+		return false;
+	}
+
+	@Override
+	public boolean povMoved(Controller controller, int povCode, PovDirection value) {
+		return false;
+	}
+
+	@Override
+	public boolean xSliderMoved(Controller controller, int sliderCode, boolean value) {
+		return false;
+	}
+
+	@Override
+	public boolean ySliderMoved(Controller controller, int sliderCode, boolean value) {
+		return false;
+	}
+
+	@Override
+	public boolean accelerometerMoved(Controller controller, int accelerometerCode, Vector3 value) {
+		return false;
 	}
 }
